362971688db7265c4e163c343fce84d52f8c3a37
tusharkhachane <70630563+tusharkhachane@users.noreply.github.com>
2022-05-20 11:53:31 +0530

    Update startApplication.sh

--- Dependency_Scripts/startApplication.sh
+++ Dependency_Scripts/startApplication.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 cd /home/ubuntu
 for pid in $(lsof -t -i:5000); do                        kill -9 $pid;                done
+rm -rf target/
+rm -rf order_service.zip
 aws s3 cp s3://java-maven/order_service.zip .
 unzip order_service.zip
 cd target/
