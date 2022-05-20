46f26adf42efaa896f4f2a4ed60be8827f39a51d
tusharkhachane <70630563+tusharkhachane@users.noreply.github.com>
2022-05-20 11:22:20 +0530

    Update startApplication.sh

--- Dependency_Scripts/startApplication.sh
+++ Dependency_Scripts/startApplication.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /home/ubuntu
 aws s3 cp s3://java-maven/order_service.zip .
-unzip -ox order_service.zip .
+unzip order_service.zip
 cd target/
 nohup java -jar order-service.jar  > /dev/null 2>&1 &
